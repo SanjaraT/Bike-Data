7ae1b0b98117470fcb55a9ea883b91433d3f2b91
sanjaraT <sanjaratunola@gmail.com>
2026-01-06 20:24:22 +0600

    Preprocessing_1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("SeoulBikeData.csv", encoding="latin1")
+df = pd.read_csv("SeoulBikeData.csv", encoding="latin1").drop(["Date","Holiday","Seasons"],axis = 1)
+#print(df.head())
+df.info()
+df["Functioning Day"] = (df["Functioning Day"]=="Yes").astype(int)
 print(df.head())
