d765a3e9e7711bdb21d951c451d32baa19b75eed
sanjaraT <sanjaratunola@gmail.com>
2026-01-07 21:32:43 +0600

    Data Visualization

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv("SeoulBikeData.csv", encoding="latin1").drop(["Date","Holiday","Seasons"],axis = 1)
 #print(df.head())
 df.info()
 df["Functioning Day"] = (df["Functioning Day"]=="Yes").astype(int)
-print(df.head())
+#print(df.head())
+
+#encoding categorical features
+le = LabelEncoder()
+for col in df.select_dtypes(include='object').columns:
+    df[col]= le.fit_transform(df[col])
+
+target = 'Rented Bike Count'
+
+#scatter plot
+for col in df.columns:
+    if col != target:
+        plt.figure()
+        plt.scatter(df[col],df[target], alpha = 0.3)
+        plt.xlabel(col)
+        plt.ylabel(target)
+        plt.title(f"{col} vs {target}")
+        plt.show()
